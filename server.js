a7104fc45c0726dc0d826e0c30cb2a42f105cf95
wisdomspine <sabopriest@gmail.com>
2019-08-18 20:17:05 +0100

    Set up start script and Heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const path= require('path');
 const fs= require('fs');
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
 const ejs= require('ejs');
 
 const MAINTENANCE_MODE=true;
